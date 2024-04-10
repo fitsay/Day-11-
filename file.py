65e658936fafae7abc075ac183f95528b9f87b9b
fitsay <163635948+fitsay@users.noreply.github.com>
2024-04-10 21:25:57 +0300

    file.py

---
+++ file.py
@@ -0,0 +1,15 @@
+def leap_year(year):
+  if year % 4 == 0:
+    if year % 100 != 0:
+      if year % 400 == 0:
+        print(year, == "Is leap year")
+      else:
+        print("Not leap year")
+    else:
+      ("Not leap year")
+  else:
+    ("Not leap year")
+def days_in_month(days):
+  days(31,28,31,30,31,30,31,31,30,30,28,30)
+year = input("insert the year")
+day = input("what is the day")
